397288b4f5c233bf79ce2f21a6ff276cc3a7b57e
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-25 09:43:16 -0400

    connected resume

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Title from './components/title/Title';
 import About from './components/about/About';
+import Resume from './components/resume/Resume';
 
 function App() {
   return (
     <>
       <Title />
       <About />
+      <Resume />
     </>
   );
 }
