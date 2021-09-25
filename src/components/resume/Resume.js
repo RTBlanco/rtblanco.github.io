4387ec0d4daa4754fb0ca3876f6abf7f132a5670
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-25 09:33:34 -0400

    created the resume component

---
+++ src/components/resume/Resume.js
@@ -0,0 +1,16 @@
+import './Resume.css';
+
+
+const Resume = () => {
+  return (
+    <div id="resume">
+      <img src="" alt="resume" />
+      <div id="resume-content">
+        <h1>RESUME</h1>
+        <button>Download</button>
+      </div>
+    </div>
+  )
+}
+
+export default Resume; 
