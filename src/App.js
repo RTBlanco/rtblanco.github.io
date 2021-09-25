4f56bf99d243cca754e2ab10a689dff35f8efe8b
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-25 09:27:12 -0400

    created the about section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Title from './components/title/Title';
+import About from './components/about/About';
 
 function App() {
   return (
     <>
       <Title />
+      <About />
     </>
   );
 }
---
+++ src/components/about/About.js
@@ -0,0 +1,16 @@
+import './About.css'
+import image from '../../ images/photo_of_me.jpg';
+
+const About = () => {
+  return (
+    <div id="about">
+      <div id="about-content">
+        <h1>ABOUT</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias magni architecto tempora minima esse. Unde error ipsam dolor magni iusto quibusdam debitis? Sit consectetur suscipit, voluptate alias commodi consequuntur enim perspiciatis eum quisquam dolore deleniti incidunt, veniam, consequatur optio adipisci deserunt harum a? Alias at ipsa recusandae placeat maxime, cupiditate earum delectus, nam reiciendis exercitationem, harum excepturi. Similique molestias consequatur laborum voluptatum eaque exercitationem. Id sequi perferendis illo, autem nulla quaerat doloremque voluptate velit? Obcaecati totam alias corrupti dicta odio consequatur beatae velit eaque vel reprehenderit veniam aut suscipit quam repellendus eius fugiat nihil quis voluptates, modi sit, cum necessitatibus?</p>
+      </div>
+      <img id="photo-me" src={image} alt="ronny" />
+    </div>
+  )
+}; 
+
+export default About;
