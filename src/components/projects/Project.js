eab2c0dd806c7c41863b35df4d677b277c542c35
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-26 19:51:44 -0400

    added comment for future changes

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -2,10 +2,11 @@ import './Project.css';
 
 const Project = ({project}) => {
   return (
+    // Make this flip to show the detail and make it linked to the github repo
     <div className="project">
       <img src="" alt="" />
       <div className="project-desc">
-        {project.desc}
+        {/* {project.desc} */}
       </div>
     </div>
   )
