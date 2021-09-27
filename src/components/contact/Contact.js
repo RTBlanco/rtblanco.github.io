0f702f4cd03efaa4b4d44933d49de80469bfdf46
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-27 10:56:08 -0400

    add an id to the form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,9 +2,9 @@ import './Contact.css';
 
 const Contact = () => {
   return (
-    <div className="contact">
+    <div id="contact">
       <h1>CONTACT</h1>
-      <form>
+      <form id='contact-form'>
         <input type="text" name='from' id='from' placeholder="From"/>
         <input type="text" name="subject" id="subject" placeholder="Subject"/>
         <textarea name="Message" id="Message" cols="30" rows="10" placeholder="Message"></textarea>
