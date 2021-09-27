119b70cebd26355c1ce4557f96fefe39d68ad4c1
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-26 20:26:11 -0400

    made the projects flip when being hoverd

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -4,9 +4,17 @@ const Project = ({project}) => {
   return (
     // Make this flip to show the detail and make it linked to the github repo
     <div className="project">
-      <img src="" alt="" />
-      <div className="project-desc">
-        {/* {project.desc} */}
+      <div className="project-inner">
+        <div className="front">
+          <img className="project-image" src="" alt="" />
+        </div>
+        
+        <div className="back">
+          <div className="project-desc">
+            {project.desc}
+          </div>
+        </div>
+        
       </div>
     </div>
   )
