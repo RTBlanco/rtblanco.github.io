d23e6a23f074ec8e75cd42f87d4bc27b3a6228aa
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-27 11:19:14 -0400

    changed the background color to match whats on figma

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,5 +1,6 @@
 import './Contact.css';
-
+// could create a back end to be able to send the info using a free hosting service
+// since its not going to matter if its fast or not 
 const Contact = () => {
   return (
     <div id="contact">
@@ -8,6 +9,7 @@ const Contact = () => {
         <input type="text" name='from' id='from' placeholder="From"/>
         <input type="text" name="subject" id="subject" placeholder="Subject"/>
         <textarea name="message" id="message" cols="30" rows="30" placeholder="Message"></textarea>
+        <button>Send</button>
       </form>
     </div>
   )
