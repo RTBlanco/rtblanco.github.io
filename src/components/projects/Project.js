11b7d3e64580028aa996f5bed2c856dcc4d3f1e4
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-26 20:54:46 -0400

    made the cards tabble

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -3,7 +3,7 @@ import './Project.css';
 const Project = ({project}) => {
   return (
     // Make this flip to show the detail and make it linked to the github repo
-    <div className="project">
+    <div tabIndex="0" className="project">
       <div className="project-inner">
         <div className="front">
           <img className="project-image" src="" alt="" />
