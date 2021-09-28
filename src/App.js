dccbc05279cd3bb4350dd0e63fef7dbba9cad4b0
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-28 11:43:44 -0400

    wrote start code

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './components/about/About';
 import Resume from './components/resume/Resume';
 import Projects from './components/projects/Projects';
 import Contact from './components/contact/Contact';
+import Skills from './components/skills/Skills';
 
 function App() {
   return (
