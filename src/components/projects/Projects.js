5930ef924c2dc7a0bfa9f7b803780a5ad1d082f9
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-28 16:20:42 -0400

    fixed typo

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -3,7 +3,7 @@ import Project from './Project';
 import github from '../../images/github.png';
 
 const Projects = () => {
-  // could connect this with the git hub api and add the data 
+  // could connect this with the github api and add the data 
   const projects = [
     {desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla labore et molestiae laborum repudiandae, suscipit doloribus sequi recusandae laboriosam adipisci alias vitae rerum quidem sit. Molestias, odio! Accusamus quo est vero repellat autem sed cum voluptates amet, maxime inventore nam magnam atque commodi dolor voluptatum fugit necessitatibus? Quibusdam, iste delectus."},
     {desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla labore et molestiae laborum repudiandae, suscipit doloribus sequi recusandae laboriosam adipisci alias vitae rerum quidem sit. Molestias, odio! Accusamus quo est vero repellat autem sed cum voluptates amet, maxime inventore nam magnam atque commodi dolor voluptatum fugit necessitatibus? Quibusdam, iste delectus."},
