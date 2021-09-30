ea7a661cdfb4be14743f5c141a70906030ec257a
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-30 13:50:28 -0400

    wrote todo

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -5,6 +5,7 @@ import github from '../../images/github.png';
 const Projects = () => {
   // could connect this with the github api and add the data 
   // TODO: Look up the Github api 
+  // Webscrape? 
   const projects = [
     {desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla labore et molestiae laborum repudiandae, suscipit doloribus sequi recusandae laboriosam adipisci alias vitae rerum quidem sit. Molestias, odio! Accusamus quo est vero repellat autem sed cum voluptates amet, maxime inventore nam magnam atque commodi dolor voluptatum fugit necessitatibus? Quibusdam, iste delectus."},
     {desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla labore et molestiae laborum repudiandae, suscipit doloribus sequi recusandae laboriosam adipisci alias vitae rerum quidem sit. Molestias, odio! Accusamus quo est vero repellat autem sed cum voluptates amet, maxime inventore nam magnam atque commodi dolor voluptatum fugit necessitatibus? Quibusdam, iste delectus."},
