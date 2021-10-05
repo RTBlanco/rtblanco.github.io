930b0dcf7253cd02dfc1dce854af34cbc67ddebf
Ronny Toribio Blanco <ronnytoribio1@hotmail.com>
2021-10-05 19:51:58 -0400

    wrote todo for the future

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,6 +6,7 @@ const About = () => {
     <div id="about">
       <div id="about-content">
         <h1>ABOUT ME</h1>
+        {/* need to add an update about picture */}
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias magni architecto tempora minima esse. Unde error ipsam dolor magni iusto quibusdam debitis? Sit consectetur suscipit, voluptate alias commodi consequuntur enim perspiciatis eum quisquam dolore deleniti incidunt, veniam, consequatur optio adipisci deserunt harum a? Alias at ipsa recusandae placeat maxime, cupiditate earum delectus, nam reiciendis exercitationem, harum excepturi. Similique molestias consequatur laborum voluptatum eaque exercitationem. Id sequi perferendis illo, autem nulla quaerat doloremque voluptate velit? Obcaecati totam alias corrupti dicta odio consequatur beatae velit eaque vel reprehenderit veniam aut suscipit quam repellendus eius fugiat nihil quis voluptates, modi sit, cum necessitatibus?</p>
       </div>
       {/* This is the reall photo that is going to be used */}
