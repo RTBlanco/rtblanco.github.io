3a64610864ba007682b9df4d01f9d74e8393e976
RTBlanco <ronnytoribio1@hotmail.com>
2021-10-08 19:33:36 -0400

    prevent the page from reloading on submit

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,10 +2,16 @@ import './Contact.css';
 // could create a back end to be able to send the info using a free hosting service
 // since its not going to matter if its fast or not 
 const Contact = () => {
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    window.alert('Messege should send')
+  }
+
   return (
     <div id="contact">
       <h1>CONTACT</h1>
-      <form id='contact-form'>
+      <form id='contact-form' onSubmit={handelSubmit}>
         <input type="text" name='from' id='from' placeholder="From"/>
         <input type="text" name="subject" id="subject" placeholder="Subject"/>
         <textarea name="message" id="message" cols="30" rows="30" placeholder="Message"></textarea>
