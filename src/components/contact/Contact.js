6bf4be363c3284b1b51d5376bcd256348d11fc27
RTBlanco <ronnytoribio1@hotmail.com>
2021-10-17 16:03:15 -0400

    updated the contacts page to be able to send messages

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,18 +1,25 @@
 import './Contact.css';
-// could create a back end to be able to send the info using a free hosting service
-// since its not going to matter if its fast or not 
+import emailjs from 'emailjs-com';
+import { useRef } from 'react';
+
 const Contact = () => {
+
+  const form = useRef(null)
 
   const handelSubmit = (e) => {
     e.preventDefault();
-    window.alert('Messege should send')
+    emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMP_ID, form.current, process.env.REACT_APP_USER_ID)
+      .catch(error => console.log(error))
+    form.current.reset()
+    window.alert('Messege was sent')
   }
 
   return (
     <div id="contact">
       <h1>CONTACT</h1>
-      <form id='contact-form' onSubmit={handelSubmit}>
-        <input type="text" name='from' id='from' placeholder="From"/>
+      <form ref={form} id='contact-form' onSubmit={handelSubmit}>
+        <input type="text" name='from_name' id='from_name' placeholder="Name"/>
+        <input type="text" name="email" id="email" placeholder="Email"/>
         <input type="text" name="subject" id="subject" placeholder="Subject"/>
         <textarea name="message" id="message" cols="30" rows="30" placeholder="Message"></textarea>
         <button>Send</button>
