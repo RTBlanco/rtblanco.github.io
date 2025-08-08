ec8fae430b4880e1e77d63bc2f58a1d15421f1ce
RTBlanco <ronnytoribio1@hotmail.com>
2025-08-08 12:48:20 -0400

    added text for the navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,7 +5,13 @@ const Navbar = () => {
 
   return (
     <div className="Navbar">
-      Navbar
+      <h1>Ronny</h1>
+      <div> 
+        <a href="">About Me</a>
+        <a href="">Projects</a>
+        <a href="">Resume</a>
+        <a href="">Contact me</a>
+      </div>
     </div>
   )
 }
