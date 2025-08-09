df4f783ceff5edb7484fa43aa01e9fbf3dec4e2d
RTBlanco <ronnytoribio1@hotmail.com>
2025-08-09 08:48:31 -0400

    feat: enhance Projects component with Swiper for mobile support; update styles for better responsiveness

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -8,21 +8,8 @@ const Project = ({project, isMobile}) => {
 
   
   return (
-    <div tabIndex="0" className="project">
-      {/* <div className="project-inner">
-        <div className="front">
-          <h1>{project.repo}</h1>
-        </div>
-        
-        <div className="back">
-          <div className="project-desc">
-            <p>{project.description}</p>
-            <a href={project.link} target="_blank" rel="noreferrer"><img src={githubBlack} alt="link to repo" /></a>
-          </div>
-        </div>
-        
-      </div> */}
 
+    <div tabIndex="0" className="project">
       <div className="icons">
         <img src={folderWhite} alt="" />
         <a href={project.link}>
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -3,6 +3,11 @@ import Project from './Project';
 // import github from '../../images/github.png';
 import { useMediaQuery } from 'react-responsive';
 import { useState, useEffect } from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+
+
+import "swiper/css";
+// import "./styles.css";
 
 
 
@@ -17,32 +22,45 @@ const Projects = () => {
   }, []);
 
 
-  const fetchProjects = () => {
-    // convert to async await in the future 
-    return fetch('https://gh-pinned-repos-tsj7ta5xfhep.deno.dev/?username=RTBlanco')
-      .then(resp => resp.json())
-      .then(data => setRepos(data))
-      .catch(error => console.log(error))
+  const fetchProjects = async () => {
+    try {
+      const resp = await fetch('https://gh-pinned-repos-tsj7ta5xfhep.deno.dev/?username=RTBlanco');
+      const data = await resp.json();
+      setRepos(data);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   
   const renderProjects = () => {
-    return repos.map((project, index) => <Project key={index} project={project} isMobile={isMobile}/> )
+    return repos.map((project, index) => {
+      if (isMobile) {
+        return (
+          <SwiperSlide key={index}>
+            <Project project={project} isMobile={isMobile}/>
+          </SwiperSlide>
+        );
+      } else {
+        return (
+          <Project key={index} project={project} isMobile={isMobile}/>
+        );
+      }
+    });
   }
 
-  console.log("repos->",repos)
   return (
     <div id="projects-container">
       <h1>PROJECTS</h1>
-      {/* <div id="outer" >
-        <div id="projects" >
-          {renderProjects()}
-        </div>
-      </div> */}
       <div id="projects">
-        {renderProjects()}
+        {isMobile ? (
+          <Swiper>
+            {renderProjects()}
+          </Swiper>
+        ) : (
+          renderProjects()
+        )}
       </div>
-      
     </div>
   )
 };
